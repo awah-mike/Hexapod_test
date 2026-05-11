793ee80b9e9ebe9d6976ab61137629741bdd745c
awah-mike <a@gramcorporation.com>
2026-05-11 23:59:21 +0000

    Register hexapod tasks on package import

--- source/hexapod_lab/hexapod_lab/__init__.py
+++ source/hexapod_lab/hexapod_lab/__init__.py
@@ -2,3 +2,6 @@
 # SPDX-License-Identifier: Apache-2.0
 
 """Isaac Lab extension package for the custom hexapod."""
+
+# Register Gym environments when the extension package is imported.
+from .tasks import *  # noqa: F401, F403
